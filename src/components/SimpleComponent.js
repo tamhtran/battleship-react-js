d63bf691622ac21f7eefa9cb4c56248e7c249bd8
ttran <ms.tam.h.tran@gmail.com>
2023-11-30 15:33:48 -0800

    install tests and organize directory

---
+++ src/components/SimpleComponent.js
@@ -0,0 +1,8 @@
+// SimpleComponent.js
+import React from 'react';
+
+function SimpleComponent() {
+  return <div>Hello, Jest!</div>;
+}
+
+export default SimpleComponent;
---
+++ src/components/SimpleComponent.test.js
@@ -0,0 +1,10 @@
+// SimpleComponent.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SimpleComponent from './SimpleComponent';
+
+test('renders a greeting message', () => {
+  render(<SimpleComponent />);
+  const greetingElement = screen.getByText('Hello, Jest!');
+  expect(greetingElement).toBeInTheDocument();
+});
